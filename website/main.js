b780e592e7d580260accc5462ce50587805ff54a
NiseVoid <nisevoid@gmail.com>
2017-02-06 21:44:20 +0100

    Retrieve data from server; Clean up some code

--- website/main.js
+++ website/main.js
@@ -4,79 +4,32 @@ Chart.defaults.scale.ticks.beginAtZero = true;
 
 var counter = 0;
 
-var windFChart = document.getElementById('windForceChart');
 var tempChart = document.getElementById('temperatureChart');
-var highestPChart = document.getElementById('highestPeakChart');
 
 var tempData = [20.1, 22, 23, 21.3, 19.8, 18.6, 19.2];
-var windForceData = [];
-var windForceLabels = [];
-var highestPData = [12,10.5,11,13.3,12,];
-var highestPLables = [];
-
-var windForceChart = new Chart(windFChart, {
-	type: 'line',
-	data: {
-    labels: windForceLabels,
-    datasets: [
-        {
-            label: "Wind Force",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointHitRadius: 20,
-            data: windForceData,
-            spanGaps: false,
-        }]}});
-		
-var highestPeakChart = new Chart(highestPChart, {
-		type: 'bar',
-		data: {
-		labels: ["Station 1", "Station 2", "Station 3", "Station 4", "Station 5"],
-		datasets: [
-			{
-				label: "Amount of rain",
-				backgroundColor: [	'rgba(5,159,255,0.5)',
-									'rgba(5,159,255,0.5)',
-									'rgba(5,159,255,0.5)',
-									'rgba(5,159,255,0.5)',
-									'rgba(5,159,255,0.5)',
-								],
-				data: highestPData,
-			}
-		]
-}})
 	
 var temperatureChart = new Chart(tempChart, {
 	type: 'line',
 	data: {
-    labels: ["Today-7", "Today-6", "Today-5", "Today-4", "Today-3", "Today-2", "Today-1"],
-    datasets: [
-        {
-            label: "Temperature",
-            fill: false,
-            lineTension: 0.2,
-            backgroundColor: "rgba(15,120,192,0.4)",
-            borderColor: "rgba(15,120,192,1)",
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointHitRadius: 20,
-            data: tempData,
-            spanGaps: false,
-        }]}});
+	labels: ["Today-7", "Today-6", "Today-5", "Today-4", "Today-3", "Today-2", "Today-1"],
+	datasets: [
+		{
+			label: "Temperature",
+			fill: false,
+			lineTension: 0.2,
+			backgroundColor: "rgba(15,120,192,0.4)",
+			borderColor: "rgba(15,120,192,1)",
+			pointBorderColor: "rgba(75,192,192,1)",
+			pointBackgroundColor: "#fff",
+			pointBorderWidth: 1,
+			pointHoverRadius: 5,
+			pointHoverBackgroundColor: "rgba(75,192,192,1)",
+			pointHoverBorderColor: "rgba(220,220,220,1)",
+			pointHoverBorderWidth: 2,
+			pointHitRadius: 20,
+			data: tempData,
+			spanGaps: false,
+		}]}});
 
 	function loadPage(page){
 		var i, tabcontent, tablinks;
@@ -92,37 +45,127 @@ var temperatureChart = new Chart(tempChart, {
 		
 	document.getElementById(page).style.display = "block";
 	}
-	}
-	
-	function updateGraph(graph,data,labels){
-		data.splice(0,1);
-		data.push((Math.random()*10)+5);
-		labels.splice(0,1);
-		if(counter%2 == 0){
-			labels.push(getTime());
-		}else{
-			labels.push("");
-		}
-		graph.update();
-		counter++;
-	}
-	
-	function getTime(){
-		var d = new Date();
-		var minutes = "0"+d.getMinutes();
-		return d.getHours()+":"+minutes.slice(-2);
-	}
-	
-	function getWindForceData(){
-	var i;
-	for(i = 0; i < 200; i++){
-		windForceData.push((Math.random()*10)+5);
-		windForceLabels.push("");
-	}
 }
 	
-	getWindForceData();
-	
-	var myInterval = setInterval(updateGraph,500,windForceChart,windForceData,windForceLabels);
-	
-	
+function getTime(){
+	var d = new Date();
+	var minutes = "0"+d.getMinutes();
+	return d.getHours()+":"+minutes.slice(-2);
+}
+
+// Highest peak rain chart
+{
+
+var peakRainData = [];
+var peakRainLabels = [];
+
+function updatePeakRain() {
+	peakRainData = [];
+	peakRainLabels = [];
+	peakRainReq = new XMLHttpRequest();
+	peakRainReq.onreadystatechange = function () {
+		if (peakRainReq.readyState == 4) {
+			var json = JSON.parse(peakRainReq.responseText);
+			json.forEach(function(elem) {
+				peakRainLabels.push(elem.station);
+				peakRainData.push(elem.value);
+				peakRainChart.update()
+			});
+		}
+	}
+	peakRainReq.open("GET", "http://localhost:9090/rain/top5", true);
+	peakRainReq.send();
+}
+
+updatePeakRain();
+setInterval(updatePeakRain,30000);
+		
+var peakRainChart = new Chart('highestPeakChart', {
+		type: 'bar',
+		data: {
+		labels: peakRainLabels,
+		datasets: [
+			{
+				label: "millimeters of rain",
+				backgroundColor: [	'rgba(5,159,255,0.5)',
+									'rgba(5,159,255,0.5)',
+									'rgba(5,159,255,0.5)',
+									'rgba(5,159,255,0.5)',
+									'rgba(5,159,255,0.5)',
+								],
+				data: peakRainData,
+			}
+		]
+}})
+}
+
+
+// WindForce Chart
+{
+var windForceData = [];
+var windForceLabels = [];
+
+var windForceChart = new Chart('windForceChart', {
+	type: 'line',
+	data: {
+	labels: windForceLabels,
+	datasets: [
+		{
+			label: "Wind Force",
+			fill: false,
+			lineTension: 0.1,
+			backgroundColor: "rgba(75,192,192,0.4)",
+			borderColor: "rgba(75,192,192,1)",
+			pointBorderColor: "rgba(75,192,192,1)",
+			pointBackgroundColor: "#fff",
+			pointBorderWidth: 1,
+			pointHoverRadius: 5,
+			pointHoverBackgroundColor: "rgba(75,192,192,1)",
+			pointHoverBorderColor: "rgba(220,220,220,1)",
+			pointHoverBorderWidth: 2,
+			pointHitRadius: 20,
+			data: windForceData,
+			spanGaps: false,
+		}]}});
+
+var lastTime = Date.now();
+var totalItems = 0;
+
+function formatDate(d) {
+	return d.getFullYear() + "-" + d.getMonth() + "-" + d.getDay() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+}
+
+function updateWindForceGraph(elem) {
+	if (totalItems > 120) {
+		windForceData.splice(0,1);
+		windForceLabels.splice(0,1);
+	}
+	windForceData.push(elem.value);
+	windForceLabels.push(formatDate(new Date(elem.time*1000)));
+	lastTime = elem.time;
+	windForceChart.update();
+	totalItems++;
+}
+
+windForceReq = new XMLHttpRequest();
+windForceReq.onreadystatechange = function () {
+	if (windForceReq.readyState == 4) {
+		var json = JSON.parse(windForceReq.responseText);
+		json.forEach(updateWindForceGraph);
+	}
+}
+windForceReq.open("GET", "http://localhost:9090/windspeed/bulk", true);
+windForceReq.send();
+
+setInterval(function(){
+	windForceReq = new XMLHttpRequest();
+	windForceReq.onreadystatechange = function () {
+		if (windForceReq.readyState == 4) {
+			var json = JSON.parse(windForceReq.responseText);
+			json.forEach(updateWindForceGraph);
+		}
+	}
+	windForceReq.open("GET", "http://localhost:9090/windspeed/since?time="+Math.floor(lastTime/1000), true);
+	windForceReq.send();
+}, 5000);
+}
